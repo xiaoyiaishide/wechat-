c174672625853dbdc10994a2e50acb9c3c67f388
liuyi <xiaoyi.ai.shide@163.com>
2018-04-26 17:38:30 +0800

    component

--- wechat/pages/my/my.js
+++ wechat/pages/my/my.js
@@ -1,5 +1,22 @@
 Page({
 	data:{
+		autoplay:true,
+		sliderList:[
+			{ className:'bg-red', name:'slider1' },
+			{ className:'bg-blue', name:'slider2' },
+			{ className:'bg-green', name:'slider3' }
+		],
 		content: 'aa'
+	},
+	play:function() {
+		debugger
+		// body...
+		this.setData({
+			autoplay:!this.data.autoplay
+		})
+	},
+	change:function(argument) {
+		// body...
+		console.log('执行了滚动！');
 	}
 })
